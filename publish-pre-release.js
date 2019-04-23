d412925e51e6eae2c64dc75c056151a6bf75b4a9
Nia Evans <nia.evans@alcumusgroup.com>
2019-04-23 14:49:10 +0100

    HF-563- Script WIP

--- publish-pre-release.js
+++ publish-pre-release.js
@@ -16,8 +16,7 @@ let tagName = '';
 
 const generateTagName = async () => {
     const currentBranch = await branchHelper.getCurrentBranch(__dirname);
-    // const branchName = branchHelper.getTicketNumberFromBranch(currentBranch);
-    const branchName = 'HF563';
+    const branchName = branchHelper.getTicketNumberFromBranch(currentBranch);
 
     const packageJSON = JSON.parse(fs.readFileSync(`${process.cwd()}/package.json`, 'utf8'));
     const currentVersion = _.get(packageJSON, 'version');
