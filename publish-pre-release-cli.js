f5d4548c189406a070b1c267122526739b933e6a
Jamie Naylor <jamie@naylorwebsites.co.uk>
2019-05-20 12:22:55 +0100

    add semi-colon!

--- publish-pre-release-cli.js
+++ publish-pre-release-cli.js
@@ -57,7 +57,7 @@ const setupGitBranch = async () => {
 };
 
 const publishVersion = async () => {
-    const npmcmd = os.platform() === 'win32' ? 'npm.cmd' : 'npm'
+    const npmcmd = os.platform() === 'win32' ? 'npm.cmd' : 'npm';
     await series (
         [npmcmd, 'publish', '--tag', tagName],
         ['git', 'tag', '-am', `Pre-release of version ${newVersion}`, tagName],
