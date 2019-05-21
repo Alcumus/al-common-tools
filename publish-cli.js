bbcb6da085137db1fed2ad96a8f4d65fdb893aed
Dave Cozens <davecozens@users.noreply.github.com>
2019-05-21 11:50:55 +0100

    Revert "Hf 773 publish not working"

--- publish-cli.js
+++ publish-cli.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 const { runShellVerbose } = require('./run.js');
-const os = require('os');
 
 const versionArguments = process.argv.slice(2);
 if (!(versionArguments[0] || '').match(/^([0-9\\.]+|major|minor|patch|premajor|preminor|prepatch|prerelease|from-git)$/)) {
@@ -16,14 +15,14 @@ const series = async (...commands) => {
 
 const version = require(`${process.cwd()}/package.json`).version;
 const tagName = `release/v${version}`;
-const npmcmd = os.platform() === 'win32' ? 'npm.cmd' : 'npm';
+
 series(
     ['git', 'update-index', '--refresh'],
     ['git', 'fetch'],
     ['git', 'diff-index', '--quiet', 'HEAD', '--'], // Ensure there are no local changes.
-    [npmcmd, 'publish'],
+    ['npm', 'publish'],
     ['git', 'tag', '-am', `Release of version ${version}`, tagName],
-    [npmcmd, '--no-git-tag-version', 'version', ...versionArguments],
+    ['npm', '--no-git-tag-version', 'version', ...versionArguments],
     ['git', 'commit', '-am', `[AUTOMATED] Updating version numbers after release of version ${version}.`],
     ['git', 'push'],
     ['git', 'push', 'origin', tagName]
