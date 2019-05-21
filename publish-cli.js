18dd360cc2db96297d5d8e1afcf60c1131b8576b
Nabeel Amir <Nabeel.Amir@alcumusgroup.com>
2019-05-21 11:42:55 +0100

    HF-773 - added check for os version in publish-cli

--- publish-cli.js
+++ publish-cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const { runShellVerbose } = require('./run.js');
+const os = require('os');
 
 const versionArguments = process.argv.slice(2);
 if (!(versionArguments[0] || '').match(/^([0-9\\.]+|major|minor|patch|premajor|preminor|prepatch|prerelease|from-git)$/)) {
@@ -15,14 +16,14 @@ const series = async (...commands) => {
 
 const version = require(`${process.cwd()}/package.json`).version;
 const tagName = `release/v${version}`;
-
+const npmcmd = os.platform() === 'win32' ? 'npm.cmd' : 'npm';
 series(
     ['git', 'update-index', '--refresh'],
     ['git', 'fetch'],
     ['git', 'diff-index', '--quiet', 'HEAD', '--'], // Ensure there are no local changes.
     ['npm', 'publish'],
     ['git', 'tag', '-am', `Release of version ${version}`, tagName],
-    ['npm', '--no-git-tag-version', 'version', ...versionArguments],
+    [npmcmd, '--no-git-tag-version', 'version', ...versionArguments],
     ['git', 'commit', '-am', `[AUTOMATED] Updating version numbers after release of version ${version}.`],
     ['git', 'push'],
     ['git', 'push', 'origin', tagName]
