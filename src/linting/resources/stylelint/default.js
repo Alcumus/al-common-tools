b63deb255c9adc9578c73481ded066bd455fc3c8
Nia Evans <nia.evans@alcumusgroup.com>
2019-06-10 08:55:53 +0100

    Editing rules to allow @extends

--- src/linting/resources/stylelint/default.js
+++ src/linting/resources/stylelint/default.js
@@ -5,6 +5,8 @@ module.exports = {
     ],
     rules: {
         indentation: 4,
-        'scss/selector-no-redundant-nesting-selector': true
+        'scss/selector-no-redundant-nesting-selector': true,
+        'at-rule-no-unknown': null,
+        'scss/at-rule-no-unknown': true
     }
 };
